2131799a6533467d7a284022dc6e8defd721e718
Dmitry Malikov <dmalikov@tms.ninja>
2023-02-01 11:37:25 +0200

    fix folders and add front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// @ts-ignore
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./modules/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'books',
+    loadChildren: () =>
+      import('./modules/books/books.module').then((m) => m.BooksModule),
+  },
+  {
+    path: 'news',
+    loadChildren: () =>
+      import('./modules/news/news.module').then((m) => m.NewsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/modules/core/core.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+
+@NgModule({
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink],
+  exports: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, SidebarComponent, FooterComponent],
+  providers: [],
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only',
+      );
+    }
+  }
+}
